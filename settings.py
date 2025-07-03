97a931995dbcb057587e63901923fd9ebc96f0f5
HuzaifaIrfan <huzaifairfan2001@gmail.com>
2025-07-03 16:16:27 +0500

    default settings

--- settings.py
+++ settings.py
@@ -4,11 +4,11 @@ from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    chatwoot_url: str = Field(..., json_schema_extra={"env": "chatwoot_url"})
-    chatwoot_ws_url: str = Field(..., json_schema_extra={"env": "chatwoot_ws_url"})
-    # access_token: str = Field(..., json_schema_extra={"env": "access_token"})
-    inbox_identifier: str = Field(..., json_schema_extra={"env": "inbox_identifier"})
-    contact_identifier: str = Field(..., json_schema_extra={"env": "contact_identifier"})
+    chatwoot_url: str = Field(default="http://chatwoot.home:3000/", json_schema_extra={"env": "chatwoot_url"})
+    chatwoot_ws_url: str = Field(default="ws://chatwoot.home:3000/", json_schema_extra={"env": "chatwoot_ws_url"})
+    # access_token: str = Field(default="" json_schema_extra={"env": "access_token"})
+    inbox_identifier: str = Field(default="", json_schema_extra={"env": "inbox_identifier"})
+    contact_identifier: str = Field(default="", json_schema_extra={"env": "contact_identifier"})
 
     model_config = SettingsConfigDict(
         env_file = ".env",
